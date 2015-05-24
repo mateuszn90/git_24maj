a4f163fab1660d1d13870d8ae46f963a3669a802
Mateusz <mati@wp.pl>
2015-05-24 14:07:39 +0200

    scalona ostateczna wersja pliku

--- kalkulator1.py
+++ kalkulator1.py
@@ -1,11 +1,14 @@
-def dodawanie(a, b):
+def dodawanie(a,b):
 	return a+b
+
 def get_info():
 	print("To jest prosty kalkulator")
+
 get_info()
 print('Podaj pierwszą liczbę')
 a = int(input())
 print('Podaj drugą liczbę')
 b = int(input())
+
 print(dodawanie(a,b))
 
